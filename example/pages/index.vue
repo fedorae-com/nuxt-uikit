169e4330b3848d9d726ed1fc8cce793d9d1d9c92
Yisrae <yisrae@fedorae.com>
2022-09-23 09:59:59 +0100

    Lint files

--- example/pages/index.vue
+++ example/pages/index.vue
@@ -3,13 +3,15 @@
     <div class="uk-container">
       <div class="uk-margin-top">
         <div class="uk-card uk-card-default uk-card-body uk-width-1-1@m">
-          <h3 class="uk-card-title">UIkit module for Nuxt.js</h3>
+          <h3 class="uk-card-title">
+            UIkit module for Nuxt.js
+          </h3>
           <p>UIkit is a lightweight and modular front-end framework for developing fast and powerful web interfaces.</p>
           <p>For more information on UIkit, check out the <a href="https://getuikit.com/docs/introduction" target="_blank" rel="noopener noreferrer">documentation</a>.</p>
           <p> Find a bug? Report it on the github <a href="https://github.com/uikit/uikit/issues" target="_blank" rel="noopener noreferrer" title="contribute">issue board</a>.</p>
           <p>Thank you for using this Nuxt UIkit module.</p>
           <div class="uk-margin-small-bottom">
-            <a href="https://github.com/fedorae-com/nuxt-uikit-module" target="_blank" rel="noopener noreferrer" class="uk-icon-button" uk-icon="github"></a>
+            <a href="https://github.com/fedorae-com/nuxt-uikit-module" target="_blank" rel="noopener noreferrer" class="uk-icon-button" uk-icon="github" />
           </div>
           <div class="text-xs-right">
             <em><small>&mdash; Yisrae</small></em>
@@ -27,8 +29,8 @@
 <script>
 export default {
   name: 'IndexPage',
-  mounted() {
+  mounted () {
     this.$UIkit.notification('UIkit + Nuxt.js')
   }
-};
+}
 </script>
