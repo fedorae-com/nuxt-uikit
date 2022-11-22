da37288fd94413f372b97d9c28ef3bfe233c4487
Yisrae <yisrae@fedorae.com>
2022-11-22 17:13:12 +0000

    Delete types definitions file

--- index.d.ts
+++
@@ -1,9 +0,0 @@
-// Type definitions for nuxt-uikit 2.0
-// Project: https://github.com/fedorae-com/nuxt-uikit
-// Definitions by: Shema <https://github.com/yisrae>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 4.8
-
-import NuxtUIkit from './lib/module';
-
-export default NuxtUIkit;
