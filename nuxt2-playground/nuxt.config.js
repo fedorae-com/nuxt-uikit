82c41acb09a50eca9e8579d25d2b16633cc0e4f5
Yisrae <yisrae@fedorae.com>
2022-11-22 17:18:23 +0000

    Add nuxt2 playground

---
+++ nuxt2-playground/nuxt.config.js
@@ -0,0 +1,11 @@
+import localModule from '../src/module'
+
+export default {
+  /*
+   ** Nuxt modules
+   */
+  buildModules: [
+    // Doc: https://github.com/fedorae-com/nuxt-uikit
+    localModule
+  ]
+}
