bebf0cf02c5b9c0dc8a7934056a9f6312bea3e37
Yisrae <yisrae@fedorae.com>
2023-01-08 19:13:57 +0000

    docs: update app config

--- docs/app.config.ts
+++ docs/app.config.ts
@@ -1,5 +1,34 @@
 export default defineAppConfig({
   docus: {
-    title: 'Nuxt UIkit'
+    title: 'Nuxt UIkit',
+    url: 'https://nuxt-uikit.fedorae.com',
+    description: 'UIkit module for Nuxt.',
+    image: '/cover.png',
+    socials: {
+      twitter: 'fedorae_',
+      github: 'fedorae-com/nuxt-uikit/tree/next',
+    },
+    aside: {
+      level: 0,
+      exclude: []
+    },
+    header: {
+      title: 'Nuxt UIkit',
+      // logo: true,
+      showLinkIcon: true,
+      exclude: []
+    },
+    footer: {
+      iconLinks: [
+        {
+          href: 'https://nuxt.com',
+          icon: 'IconNuxtLabs'
+        },
+        {
+          href: 'https://fedorae.com',
+          icon: ''
+        }
+      ]
+    }
   }
 })
