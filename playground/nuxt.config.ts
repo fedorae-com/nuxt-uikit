54f2a5985745d2152fcafd4eefdfbd9cd7c624fa
Yisrae <yisrae@fedorae.com>
2023-02-13 03:30:51 +0000

    Simplified nuxt.config

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,12 +1,3 @@
-import { defineNuxtConfig } from 'nuxt/config'
-import uikitModule from '../src/module'
-
 export default defineNuxtConfig({
-  modules: [
-    // @ts-ignore
-    uikitModule
-  ],
-  uikit: {
-    addPlugin: true
-  }
+  modules: ['../src/module'],
 })
