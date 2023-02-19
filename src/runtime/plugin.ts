4fa321dc700744d48b70af5fbc4c517de096daa1
Yisrae <yisrae@fedorae.com>
2023-02-19 10:35:33 +0000

    Comment uikit plugin config

--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -1,11 +1,9 @@
 import { defineNuxtPlugin } from '#app'
-import UIkit from 'uikit'
-import Icons from 'uikit/dist/js/uikit-icons'
-
-UIkit.use(Icons)
+// import UIkit from 'uikit'
+// import Icons from 'uikit/dist/js/uikit-icons'
 
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.$UIkit = UIkit
-  nuxtApp.$UIkit.use(Icons)
-  nuxtApp.provide('UIkit', UIkit)
+  // nuxtApp.$UIkit = UIkit
+  // nuxtApp.$UIkit.use(Icons)
+  // nuxtApp.provide('UIkit', UIkit)
 })
