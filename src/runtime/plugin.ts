311db611269527ac4645a4b43ce1eb5d996d7145
Yisrae <yisrae@fedorae>
2023-06-21 10:00:11 +0100

    Remove zombie code

--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -1,9 +1,3 @@
 import { defineNuxtPlugin } from '#app'
-// import UIkit from 'uikit'
-// import Icons from 'uikit/dist/js/uikit-icons'
 
-export default defineNuxtPlugin((nuxtApp) => {
-  // nuxtApp.$UIkit = UIkit
-  // nuxtApp.$UIkit.use(Icons)
-  // nuxtApp.provide('UIkit', UIkit)
-})
+export default defineNuxtPlugin(() => {})
