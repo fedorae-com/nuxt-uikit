ef256d36cbaed35afce271de8243184b9f82373d
swalx <swalx@fedorae>
2024-06-17 11:05:59 +0100

    test: add simple test for GitHub icon element

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -1,15 +1,33 @@
 import { describe, it, expect } from 'vitest'
 import { fileURLToPath } from 'node:url'
 import { setup, $fetch } from '@nuxt/test-utils'
+import { mount } from '@vue/test-utils'
 
-describe('ssr', async () => {
+// @ts-ignore
+import App from '../test/fixtures/basic/app.vue'
+
+// @vitest-environment happy-dom
+
+describe('main', async () => {
+  const baseUrl = import.meta.url
+  const filePath = new URL('./fixtures/basic', baseUrl)
+
   await setup({
-    rootDir: fileURLToPath(new URL('./fixtures/basic', import.meta.url)),
+      rootDir: fileURLToPath(filePath)
   })
 
-  it('renders the index page', async () => {
+  it('renders the GitHub icon', async () => {
     // Get response to a server-rendered page with `$fetch`.
     const html = await $fetch('/')
-    expect(html).toContain('<div>basic</div>')
+
+    const wrapper = mount(App)
+    const element = wrapper.find('#iconEl')
+    expect(element.exists()).toBeTruthy()
+
+    // element has class
+    expect(element.classes()).toEqual(expect.arrayContaining(['uk-icon-button']))
+
+    // Offcanvas is shown
+    expect(html).toContain('<h3>Title</h3>')
   })
 })
--- test/fixtures/basic/app.vue
+++ test/fixtures/basic/app.vue
@@ -1,6 +1,97 @@
-<template>
-  <div>basic</div>
-</template>
-
 <script setup>
+import { onMounted } from 'vue'
+import UIkit from 'uikit'
+onMounted(() => {
+  UIkit.notification({ message: 'Nuxt + UIkit', status: 'success' })
+  const iconEl = UIkit.icon('#iconEl')
+  UIkit.notification({ message: `Icon: ${iconEl}`, status: 'primary' })
+  UIkit.offcanvas('#offcanvas-overlay', { overlay: true })
+})
 </script>
+
+<template>
+  <div>
+    <div class="uk-container">
+      <div class="uk-margin-top">
+        <div class="uk-card uk-card-default uk-card-body uk-width-1-1@m">
+          <h3 class="uk-card-title">
+            UIkit module for Nuxt
+          </h3>
+          <p>
+            UIkit is a lightweight and modular front-end framework for
+            developing fast and powerful web interfaces.
+          </p>
+          <p>
+            For more information on UIkit, check out the
+            <a
+              href="https://getuikit.com/docs/introduction"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              documentation </a>.
+          </p>
+          <p>
+            Find a bug? Report it on the github
+            <a
+              href="https://github.com/uikit/uikit/issues"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="contribute"
+            >
+              issue board </a>.
+          </p>
+          <p>Thank you for using this UIkit module.</p>
+          <div class="uk-margin-small-bottom">
+            <a
+              id="iconEl"
+              href="https://github.com/fedorae-com/nuxt-uikit"
+              target="_blank"
+              rel="noopener noreferrer"
+              class="uk-icon-button uk-icon"
+              uk-icon="github"
+            />
+          </div>
+          <div class="text-xs-right">
+            <em><small>&mdash; Fedorae</small></em>
+          </div>
+          <hr>
+          <a
+            href="https://nuxt.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Nuxt Documentation
+          </a>
+          <br>
+          <a
+            href="https://github.com/nuxt/nuxt"
+            target="_blank"
+            rel="noopener noreferrer"
+          >Nuxt GitHub</a>
+          <br>
+          <br>
+        </div>
+      </div>
+    </div>
+
+    <!-- Offcanvas -->
+    <div id="offcanvas-overlay">
+      <div class="uk-offcanvas-bar">
+        <button
+          class="uk-offcanvas-close uk-icon uk-close"
+          type="button"
+          uk-close
+          />
+
+        <h3>Title</h3>
+
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
